48d43e7ac15c0938cd3048e37b2d7cc69ce78014
Facundo <fclarat@hotmail.com>
2016-09-07 10:56:30 -0300

    finish invites

--- futbol/app/controllers/invites_controller.rb
+++ futbol/app/controllers/invites_controller.rb
@@ -1,2 +1,13 @@
 class InvitesController < ApplicationController
+	def create
+		@event = Event.find(params[:event_id])
+		@invite = @event.invites.create(Hash["name" => invite_params['name'], "mail" => invite_params['mail'], "confirmed" => 1])
+
+		redirect_to event_path(@event)
+	end
+
+	private
+		def invite_params
+			params.require(:invite).permit(:name, :mail)
+		end
 end
