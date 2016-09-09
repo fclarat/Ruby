98bc7d64a1632f1cf045704e48df107d2d7fb3af
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-09-09 13:17:23 -0300

    testing email heroku

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -4,29 +4,29 @@ class ExampleMailer < ApplicationMailer
     def sample_email(invite)
         @invite = invite
 
-        # mail(to: @invite.mail, subject: 'Sample Email')
+        mail(to: @invite.mail, subject: 'Sample Email')
 
-        # First, instantiate the Mailgun Client with your API key
-        mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
-        mb_obj = Mailgun::MessageBuilder.new
+        # # First, instantiate the Mailgun Client with your API key
+        # mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
+        # mb_obj = Mailgun::MessageBuilder.new
 
-        # Define the from address.
-        mb_obj.from ENV['EMAIL_FROM']
+        # # Define the from address.
+        # mb_obj.from ENV['EMAIL_FROM']
         
-        # Define a to recipient.
-        mb_obj.add_recipient :to, @invite.mail
+        # # Define a to recipient.
+        # mb_obj.add_recipient :to, @invite.mail
         
-        # Define the subject.
-        mb_obj.subject "Mailgun HTML Example"
+        # # Define the subject.
+        # mb_obj.subject "Mailgun HTML Example"
         
-        # Define the body of the message.
-        # mb_obj.body_text render_to_string('/example_mailer/sample_email.text.erb', layout: false, content_type: 'text/plain').to_str
+        # # Define the body of the message.
+        # # mb_obj.body_text render_to_string('/example_mailer/sample_email.text.erb', layout: false, content_type: 'text/plain').to_str
         
-        # Define the body of the message.
-        mb_obj.body_html render_to_string('example_mailer/sample_email').to_str
-        # mb_obj.body_text "EMAIL!"
+        # # Define the body of the message.
+        # mb_obj.body_html render_to_string('example_mailer/sample_email').to_str
+        # # mb_obj.body_text "EMAIL!"
 
-        # Finally, send your message using the client
-        mg_client.send_message ENV['MAILGUN_DOMAIN'], mb_obj
+        # # Finally, send your message using the client
+        # mg_client.send_message ENV['MAILGUN_DOMAIN'], mb_obj
     end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.active_job.queue_adapter = :delayed_job
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'rubyfutbol.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
